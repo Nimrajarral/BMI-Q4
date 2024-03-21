04aba8d13c85ed62fc453431a95906cef399df63
Nimrajarral <nimrajarral@gmail.com>
2024-03-22 01:12:04 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+var weightInKg = 13;
+var heightInMeters = 9;
+var bodyMassIndex = (weightInKg / heightInMeters);
+console.log(" your BMI is ".concat(bodyMassIndex, " "));
---
+++ main.ts
@@ -0,0 +1,4 @@
+let weightInKg : number=13
+let heightInMeters:number =9
+let bodyMassIndex:number=(weightInKg/heightInMeters);
+console.log(` your BMI is ${bodyMassIndex} `);
